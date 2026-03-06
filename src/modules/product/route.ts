4d3b77e58f516f16886ae30e192fdfbd530da888
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-06 11:04:47 +0700

    fix doxker file run build

--- src/modules/product/route.ts
+++ src/modules/product/route.ts
@@ -113,6 +113,9 @@ productRoute.openapi(
         where: {
           slug,
         },
+        include: {
+          variant: true,
+        },
       });
 
       if (!product) return c.json({ message: "Product not found!", slug }, 404);
