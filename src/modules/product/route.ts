898f938f5096d2c4a4ebcbcbdd3a5b736b3890aa
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-11 07:01:08 +0000

    feat: add .env

--- src/modules/product/route.ts
+++ src/modules/product/route.ts
@@ -1,5 +1,5 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { GetProductBySlug, ProductSchema, SearchQueryParams } from "./schema";
+import { ProductSchema } from "./schema";
 import { prisma } from "../../lib/prisma";
 
 export const productRoute = new OpenAPIHono();
@@ -38,91 +38,91 @@ productRoute.openapi(
 );
 
 // Search Product
-productRoute.openapi(
-  {
-    method: "get",
-    path: "/search",
-    description: "Search products",
-    tags: tag,
-    request: {
-      query: SearchQueryParams,
-    },
-    responses: {
-      200: {
-        description: "Successfully get products",
-        content: { "application/json": { schema: ProductSchema } },
-      },
-      500: {
-        description: "Failed to get products",
-      },
-      400: {
-        description: "Product not found!",
-      },
-    },
-  },
-  async (c) => {
-    try {
-      const { q } = c.req.valid("query");
+// productRoute.openapi(
+//   {
+//     method: "get",
+//     path: "/search",
+//     description: "Search products",
+//     tags: tag,
+//     request: {
+//       query: SearchQueryParams,
+//     },
+//     responses: {
+//       200: {
+//         description: "Successfully get products",
+//         content: { "application/json": { schema: ProductSchema } },
+//       },
+//       500: {
+//         description: "Failed to get products",
+//       },
+//       400: {
+//         description: "Product not found!",
+//       },
+//     },
+//   },
+//   async (c) => {
+//     try {
+//       const { q } = c.req.valid("query");
 
-      const foundProduct = await prisma.product.findMany({
-        where: {
-          OR: [
-            { name: { contains: q, mode: "insensitive" } },
-            { slug: { contains: q, mode: "insensitive" } },
-          ],
-        },
-      });
+//       const foundProduct = await prisma.product.findMany({
+//         where: {
+//           OR: [
+//             { name: { contains: q, mode: "insensitive" } },
+//             { slug: { contains: q, mode: "insensitive" } },
+//           ],
+//         },
+//       });
 
-      if (!foundProduct) return c.json({ message: "Product not found!" }, 404);
+//       if (!foundProduct) return c.json({ message: "Product not found!" }, 404);
 
-      return c.json(foundProduct, 200);
-    } catch (error) {
-      return c.json({ message: "Failes to get Product!", error }, 500);
-    }
-  },
-);
+//       return c.json(foundProduct, 200);
+//     } catch (error) {
+//       return c.json({ message: "Failes to get Product!", error }, 500);
+//     }
+//   },
+// );
 
-// Get One Product by Slug
-productRoute.openapi(
-  {
-    method: "get",
-    path: "/{slug}",
-    description: "Get one product",
-    tags: tag,
-    request: {
-      params: GetProductBySlug,
-    },
-    responses: {
-      200: {
-        description: "Successfully get products",
-        content: { "application/json": { schema: ProductSchema } },
-      },
-      500: {
-        description: "Failed to get products",
-      },
-      400: {
-        description: "Product not found!",
-      },
-    },
-  },
-  async (c) => {
-    try {
-      const { slug } = c.req.valid("param");
+// // Get One Product by Slug
+// productRoute.openapi(
+//   {
+//     method: "get",
+//     path: "/{slug}",
+//     description: "Get one product",
+//     tags: tag,
+//     request: {
+//       params: GetProductBySlug,
+//     },
+//     responses: {
+//       200: {
+//         description: "Successfully get products",
+//         content: { "application/json": { schema: ProductSchema } },
+//       },
+//       500: {
+//         description: "Failed to get products",
+//       },
+//       400: {
+//         description: "Product not found!",
+//       },
+//     },
+//   },
+//   async (c) => {
+//     try {
+//       const { slug } = c.req.valid("param");
 
-      const product = await prisma.product.findUnique({
-        where: {
-          slug,
-        },
-        include: {
-          variant: true,
-        },
-      });
+//       const product = await prisma.product.findUnique({
+//         where: {
+//           slug,
+//         },
+//         include: {
+//           variant: true,
+//         },
+//       });
 
-      if (!product) return c.json({ message: "Product not found!", slug }, 404);
+//       if (!product) return c.json({ message: "Product not found!", slug }, 404);
 
-      return c.json(product, 200);
-    } catch (error) {
-      return c.json({ message: "Failed to get product", error }, 500);
-    }
-  },
-);
+//       return c.json(product, 200);
+//     } catch (error) {
+//       return c.json({ message: "Failed to get product", error }, 500);
+//     }
+//   },
+// );
