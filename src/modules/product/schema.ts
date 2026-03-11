a4447bc75c1ba4f1cc8f60b72395d86e4782b3c4
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-11 06:52:21 +0000

    feat: install prisma zod generator and generate schema

--- src/modules/product/schema.ts
+++ src/modules/product/schema.ts
@@ -1,54 +1,8 @@
 import z from "zod";
+import { ProductModelSchema } from "../../generated/zod/schemas";
 
-export const ProductSchema = z.object({
-  id: z.string().openapi({ example: "" }),
-  slug: z.string().min(1).max(100),
-  name: z.string().min(3).max(100).openapi({ example: "Betta Multicolor" }),
-  price: z.number().positive().openapi({ example: 100000 }),
-  stockLevel: z.number().int().nonnegative().openapi({ example: 10 }),
-  sku: z.string().min(3).max(100).openapi({ example: "BW-MC-1" }),
-  variant: z.string().min(3).max(100).openapi({ example: "Competition" }),
-  thumbnailUrl: z.url().openapi({
-    example:
-      "https://3ufa9hkbld.ucarecd.net/926645e1-07ae-4599-839c-78a8159492da/-/scale_crop/300x300/",
-  }),
-  imageUrls: z.array(z.string()),
-  createdAt: z
-    .string()
-    .min(1)
-    .max(100)
-    .openapi({ example: "2020-06-19T17:13:00.000Z" }),
-  updatedAt: z
-    .string()
-    .min(1)
-    .max(100)
-    .openapi({ example: "2020-06-19T17:13:00.000Z" }),
-});
-
-export const SeedProductSchema = ProductSchema.omit({
-  id: true,
-  slug: true,
-  createdAt: true,
-  updatedAt: true,
-});
-
-export const ProductsSchema = z.array(ProductSchema);
-export const SeedProductsSchema = z.array(SeedProductSchema);
+export const ProductSchema = ProductModelSchema;
+export const ProductsSchema = ProductSchema.array();
 
 export type Product = z.infer<typeof ProductSchema>;
-export type Products = z.infer<typeof ProductSchema>;
-
-export type SeedProductSchema = z.infer<typeof SeedProductSchema>;
-export type SeedProductsSchema = z.infer<typeof SeedProductSchema>;
-
-export const GetProductBySlug = z.object({
-  slug: z
-    .string()
-    .min(1)
-    .max(100)
-    .openapi({ example: "Multicolor-Modern-Plakat" }),
-});
-
-export const SearchQueryParams = z.object({
-  q: z.string().min(1).max(100).openapi({ example: "Modern" }),
-});
+export type Products = z.infer<typeof ProductsSchema>;
--- test.ts
+++
@@ -1,8 +0,0 @@
-async function getProducts() {
-  const response = await fetch("https://betta-wava.burhanudin.com/products");
-  const data = await response.json();
-
-  console.log(data);
-}
-
-getProducts();
